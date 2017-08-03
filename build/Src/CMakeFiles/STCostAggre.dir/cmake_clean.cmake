6a85aa7ed2a08c2edb5aaab62557f22cd2fd584e
xuezhi.zhang <xuezhi.zhang@hobot.cc>
2017-08-03 20:12:42 +0800

    init project

---
+++ build/Src/CMakeFiles/STCostAggre.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/STCostAggre.dir/Toolkit.cpp.o"
+  "CMakeFiles/STCostAggre.dir/StereoDisparity.cpp.o"
+  "CMakeFiles/STCostAggre.dir/main.cpp.o"
+  "CMakeFiles/STCostAggre.dir/ctmf.c.o"
+  "CMakeFiles/STCostAggre.dir/SegmentTree.cpp.o"
+  "CMakeFiles/STCostAggre.dir/StereoHelper.cpp.o"
+  "../bin/STCostAggre.pdb"
+  "../bin/STCostAggre"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/STCostAggre.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/Test/config.h
@@ -0,0 +1,3 @@
+#define EXE_NAME "STCostAggre"
+#define ROOT_PATH "/home/users/xuezhi.zhang/stereo/backup/STCostAggregation-master/"
+#define RUN_PATH "./STCostAggre"
